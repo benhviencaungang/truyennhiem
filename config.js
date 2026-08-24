1f49f97091a8c06364bda566cc9a984515c9f270
benhviencaungang <admin@benhviencaungang.com>
2026-08-24 17:31:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,4 @@
 const CONFIG = {
     SUPABASE_URL: 'https://ezwatizguwavzkhjobcs.supabase.co',
     SUPABASE_KEY: 'sb_publishable_DAimtimUWmrusUA9S8kv0A_8efwBFGR',
-    
-    // Tài khoản API Cổng ECDS Bộ Y Tế
-    ECDS_USER: 'bvdkkvcaungangtv', // Sửa lại thành tài khoản của bạn
-    ECDS_PASS: 'Bvcn2025@'     // Sửa lại thành mật khẩu của bạn
 };
